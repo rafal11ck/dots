8d5ceeeaceef32218f3564ef1afa7af67ea00199
Rafał <example@invalid.com>
2023-01-22 19:02:52 +0100

    anon

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Rafał Grot"
+(setq user-full-name ""
       user-mail-address "invalid@example.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
